32f378b01509ee9604bd70890a5a2773ef8cb5e9
iamb33k <benkline33@gmail.com>
2022-03-14 21:26:00 -0400

    calc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(num1, num2) {
+  let sum = num1+num2;
+  return(sum);
+};
+
+const subtract = function(num1, num2) {
+  let difference = num1-num2;
+  return(difference);
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array1) {
+  const initVal = 0;
+  const sumWithInit = array1.reduce(
+    (previousValue, currentValue) => previousValue + currentValue, initVal
+  );
+	return(sumWithInit);
 };
 
-const sum = function() {
-	
+const multiply = function(array1) {
+  const prodWithInit = array1.reduce(
+    (previousValue, currentValue) => previousValue * currentValue
+  );
+	return(prodWithInit);
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+  let result = num1 ** num2;
+  return(result);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num1) {
+  if (num1 === 1 || num1 === 0){
+    return(1);
+  }
+ for (let i = num1 - 1; i > 1; i--){
+  num1 = num1 * i;
+ }
+ return(num1);
 };
 
 // Do not edit below this line
