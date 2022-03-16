9d3d265b64e17bb460bf282cf427efd0968558bf
iamb33k <benkline33@gmail.com>
2022-03-15 20:35:30 -0400

    9, 10, 11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string1) {
+    string1 = string1.toLowerCase()
+    string1 = string1.replace(/[.,#!]/g,"")
+    string1 = string1.replace(/\s+/g,'');
+    array1 = string1.split("");
+    array1 = array1.reverse();
+    array1 = array1.join("");
+    if (string1 == array1){
+        return(true);
+    }
+    else{
+        return(false);
+    }
+}
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if ((index == 1) || (index == 2)){
+        return(1);
+    }
+    else if(index < 0){
+        return("OOPS");
+    }
+    else{
+    let num1 = 1;
+    let num2 = 1
+    let next;
+    let i = 2;
+    while (i < index){
+        next = num1 + num2;
+        num1 = num2;
+        num2 = next;
+        i = i+1;
+    }
+    return(next);  
+}
+}
 
-};
 
 // Do not edit below this line
 module.exports = fibonacci;
